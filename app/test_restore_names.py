5b4e6bff402e1fe2564360f3e49752dee4190c61
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-06-05 13:12:14 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,29 +6,29 @@ from app.restore_names import restore_names
     "users,result",
     [
         (
-                [
-                    {
-                        "first_name": None,
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy"
-                    },
-                    {
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams"
-                    },
-                ],
-                [
-                    {
-                        "first_name": "Jack",
-                        "last_name": "Holy",
-                        "full_name": "Jack Holy"
-                    },
-                    {
-                        "first_name": "Mike",
-                        "last_name": "Adams",
-                        "full_name": "Mike Adams"
-                    },
-                ]
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams"
+                },
+            ],
+            [
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams"
+                },
+            ]
         )
     ]
 )
